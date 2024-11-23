f4f57adef2dc89ff5496ba7502cf1b5864af84b7
Frank Male <frmale@utexas.edu>
2024-11-22 20:05:28 -0500

    :recycle: remove old python function

--- python/docx_comments/get_comments.py
+++ python/docx_comments/get_comments.py
@@ -2,45 +2,8 @@
 
 from __future__ import annotations
 
-import zipfile
-from pathlib import Path
-
 import click
 from docx_comments._core import get_comments
-from lxml import etree
-
-ooxml_namespaces = {"w": "http://schemas.openxmlformats.org/wordprocessingml/2006/main"}
-
-
-def get_comments_old(file_name: str | Path) -> list[dict[str, str]]:
-    """Extract comments from Word file.
-
-    Parameters
-    ----------
-    file_name : str | Path
-        location of the file
-
-    Returns
-    -------
-    list[dict[str,str]]
-        list of comments with author, date, and text gathered
-
-    References
-    ----------
-    https://stackoverflow.com/questions/47390928/extract-docx-comments
-    """
-    with zipfile.ZipFile(file_name, "r") as docx_zip:
-        comments_xml = docx_zip.read("word/comments.xml")
-    comments = etree.XML(comments_xml).xpath("//w:comment", namespaces=ooxml_namespaces)
-    comments_parsed = [
-        {
-            "author": c.xpath("@w:author", namespaces=ooxml_namespaces)[0],
-            "date": c.xpath("@w:date", namespaces=ooxml_namespaces)[0],
-            "text": c.xpath("string(.)", namespaces=ooxml_namespaces),
-        }
-        for c in comments
-    ]
-    return comments_parsed
 
 
 @click.command()
